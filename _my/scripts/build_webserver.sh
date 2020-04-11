9c20c0493150cc8841a0733509f7cbace01892ed
Mihail M <r@gothundead.ru>
2020-04-11 21:59:37 +0300

    add vhosts

---
+++ _my/scripts/build_webserver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ./../../
+docker-compose build --no-cache nginx-56 php-fpm-56 nginx-70 nginx-71 nginx-72 nginx-73 nginx-74 apache2-56 apache2-70 apache2-71 apache2-72 apache2-73 apache2-74 mysql mariadb phpmyadmin adminer phpmyadmin-mariadb
