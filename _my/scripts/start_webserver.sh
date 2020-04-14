6e96f0e0446fce148a3d1ca1e76fcc0eb9f1508a
Mihail M <r@gothundead.ru>
2020-04-14 22:04:39 +0300

    change project path

--- _my/scripts/start_webserver.sh
+++ _my/scripts/start_webserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ./../../
-docker-compose up -d nginx-56 nginx-70 nginx-71 nginx-72 nginx-73 nginx-74 apache2-56 apache2-70 apache2-71 apache2-72 apache2-73 apache2-74 mysql phpmyadmin adminer
+docker-compose up -d nginx-56 nginx-70 nginx-71 nginx-72 nginx-73 nginx-74 apache2-56 apache2-70 apache2-71 apache2-72 apache2-73 apache2-74 mysql phpmyadmin
